23fc64249f698fb2ee271afb2793ce0e64112099
samllchiang <samllchiang@hotmail.com>
2017-08-18 15:12:28 +0800

    app.jsx modify 5

--- client/app.jsx
+++ client/app.jsx
@@ -1,54 +1,122 @@
 const React = require('react');
 const ReactDOM = require('react-dom');
+const request = require('superagent');
+
+class PlaceList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { places: [] };
+  }
+
+  update() {
+    const self = this;
+    const { host, port } = window.location;
+    const url = `http://${host}/api/history`
+    request
+      .get(url)
+      .end(function (error, response) {
+        let result = JSON.parse(response.text)
+        console.log(result)
+        self.setState({
+          places: result,
+        })
+      })
+  }
+
+  componentDidMount() {
+    this.update()
+  }
+
+  render() {
+    return (
+      <div id="resultList" className="ui relaxed divided list">
+        {this.state.places.map(function (item, index) {
+          return <PlaceItem key={index} place={item.place} address={item.result.formatted_address} />
+        })}
+      </div>
+    )
+  }
+}
 
 class PlaceItem extends React.Component {
   render() {
     return (
       <div className="item">
-        <i className="large youtube middle aligned icon"></i>
+        <i className="large map signs middle aligned icon"></i>
         <div className="content">
           <a className="header">{this.props.place}</a>
+          <div className="description">{this.props.address}</div>
         </div>
       </div>
+    )
+  }
+}
+
+class Title extends React.Component {
+  render() {
+    return (
+      <h2 className="ui teal image header">
+        <i className="hand spock icon"></i>
+        <div className="content">
+          {this.props.text}
+        </div>
+      </h2>
     )
   }
 }
 
 class App extends React.Component {
+
+  constructor(props) {
+    super(props)
+    this.state = {
+      searchPlace: ''
+    }
+
+    // This binding is necessary to make `this` work in the callback
+    this.send = this.send.bind(this)
+    this.placeList = undefined
+  }
+
+  handleChange(e) {
+    this.setState({ searchPlace: e.target.value });
+  }
+
+  send() {
+    const self = this;
+    const { searchPlace } = this.state;
+    const { host, port } = window.location;
+    const url = `http://${host}/api/search-place`
+    request
+      .get(url)
+      .query({ place: searchPlace })
+      .end(function (error, response, body) {
+        let result = JSON.parse(response.text)
+        self.placeList.update()
+        console.log(result)
+      })
+  }
+
   render() {
     return (
       <div className="ui middle aligned center aligned grid">
         <div className="fourteen wide column">
-          <h2 className="ui teal image header">
-            <i className="hand spock icon"></i>
-            <div className="content">
-              Search the place
-            </div>
-          </h2>
+          <Title text="Search the place" />
           <div className="ui action input">
-            <input id="urlText" type="text" placeholder="Search..." />
-            <button id="searchButton" className="ui blue button">Search</button>
+            <input type="text" placeholder="Search..." value={this.state.searchPlace}
+              onChange={this.handleChange.bind(this)}
+            />
+            <button className="ui blue button" onClick={this.send}>Search</button>
           </div>
           <div className="ui horizontal divider">
             place-spot
           </div>
-          <div id="resultList" className="ui relaxed divided list">
-            <PlaceItem place="Taipei 101" />
-            <PlaceItem place="NTU" />
-            <PlaceItem place="師大夜市" />
-          </div>
+          <PlaceList ref={(placeList) => { this.placeList = placeList }} />
         </div>
       </div>
     )
   }
 }
-
-/*
-ReactDOM.render(
-  <h1>Hello, world!</h1>,
-  document.getElementById('root')
-);
-*/
 
 ReactDOM.render(
   <App />,
