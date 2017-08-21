1a924610ed517bc1b0b43dd2a17f83c7b140dc16
samllchiang <samllchiang@hotmail.com>
2017-08-21 10:19:43 +0800

    modify app.jsx from teacher

--- client/app.jsx
+++ client/app.jsx
@@ -2,47 +2,15 @@ const React = require('react');
 const ReactDOM = require('react-dom');
 const request = require('superagent');
 
-class PlaceList extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { places: [] };
-  }
-
-  update() {
-    const self = this;
-    const { host, port } = window.location;
-    const url = `http://${host}/api/history`
-    request
-      .get(url)
-      .end(function (error, response) {
-        let result = JSON.parse(response.text)
-        console.log(result)
-        self.setState({
-          places: result,
-        })
-      })
-  }
-
-  componentDidMount() {
-    this.update()
-  }
-
-  render() {
-    return (
-      <div id="resultList" className="ui relaxed divided list">
-        {this.state.places.map(function (item, index) {
-          return <PlaceItem key={index} place={item.place} address={item.result.formatted_address} />
-        })}
-      </div>
-    )
-  }
+const getApiUrl = () => {
+  return `${window.location.protocol}//${window.location.host}`;
 }
 
 class PlaceItem extends React.Component {
   render() {
     return (
       <div className="item">
-        <i className="large map signs middle aligned icon"></i>
+        <i className="large youtube middle aligned icon"></i>
         <div className="content">
           <a className="header">{this.props.place}</a>
           <div className="description">{this.props.address}</div>
@@ -52,15 +20,39 @@ class PlaceItem extends React.Component {
   }
 }
 
-class Title extends React.Component {
+class PlaceItemList extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      places: []
+    }
+  }
+
+  componentDidMount() {
+    this.update()
+  }
+
+  update() {
+    let self = this;
+    let url = `${getApiUrl()}/api/history`;
+    request
+      .get(url)
+      .end(function (error, response) {
+        let result = JSON.parse(response.text)
+        self.setState({ places: result })
+      })
+  }
+
   render() {
     return (
-      <h2 className="ui teal image header">
-        <i className="hand spock icon"></i>
-        <div className="content">
-          {this.props.text}
-        </div>
-      </h2>
+      <div id="resultList" className="ui relaxed divided list">
+        {
+          this.state.places.map(function (value, index) {
+            return <PlaceItem key={index} place={value.place}
+              address={value.result.formatted_address} />
+          })
+        }
+      </div>
     )
   }
 }
@@ -72,28 +64,22 @@ class App extends React.Component {
     this.state = {
       searchPlace: ''
     }
-
-    // This binding is necessary to make `this` work in the callback
-    this.send = this.send.bind(this)
-    this.placeList = undefined
   }
 
-  handleChange(e) {
-    this.setState({ searchPlace: e.target.value });
+  handleChange(event) {
+    this.setState({ searchPlace: event.target.value })
   }
 
   send() {
-    const self = this;
-    const { searchPlace } = this.state;
-    const { host, port } = window.location;
-    const url = `http://${host}/api/search-place`
+    let self = this;
+    let url = `${getApiUrl()}/api/search-place`;
     request
       .get(url)
-      .query({ place: searchPlace })
-      .end(function (error, response, body) {
+      .query({ place: this.state.searchPlace })
+      .end(function (error, response) {
         let result = JSON.parse(response.text)
-        self.placeList.update()
         console.log(result)
+        self.placeItemList.update()
       })
   }
 
@@ -101,17 +87,26 @@ class App extends React.Component {
     return (
       <div className="ui middle aligned center aligned grid">
         <div className="fourteen wide column">
-          <Title text="Search the place" />
+          <h2 className="ui teal image header">
+            <i className="hand spock icon"></i>
+            <div className="content">
+              Search the place
+            </div>
+          </h2>
           <div className="ui action input">
-            <input type="text" placeholder="Search..." value={this.state.searchPlace}
-              onChange={this.handleChange.bind(this)}
-            />
-            <button className="ui blue button" onClick={this.send}>Search</button>
+            <input id="urlText" type="text" placeholder="Search..."
+              onChange={this.handleChange.bind(this)} />
+            <button id="searchButton" className="ui blue button"
+              onClick={this.send.bind(this)}>Search</button>
           </div>
           <div className="ui horizontal divider">
             place-spot
           </div>
-          <PlaceList ref={(placeList) => { this.placeList = placeList }} />
+          <PlaceItemList ref={
+            (placeItemList) => {
+              this.placeItemList = placeItemList
+            }
+          } />
         </div>
       </div>
     )
